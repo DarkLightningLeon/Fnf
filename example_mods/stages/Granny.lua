b4fb7142f2e8379a86d2843747dff39f267917bb
DarkLightningLeon <113647031+DarkLightningLeon@users.noreply.github.com>
2022-10-26 16:43:05 -0400

    Add files via upload

---
+++ example_mods/stages/Granny.lua
@@ -0,0 +1,12 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('Granny', 'Granny', 0, 0);
+	setScrollFactor('Granny', 0.9, 0.9);
+
+	
+
+	addLuaSprite('Granny', false);
+	
+	
+	close(true); 
+end
